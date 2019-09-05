b01e4176fdbad893add82bf89cf011ba9ef8d50f
Cedric Hutchings <cedhut02@gmail.com>
2019-09-05 16:08:03 -0400

    start using more versatile approach to items.

--- make/src/main.rs
+++ make/src/main.rs
@@ -1,10 +1,16 @@
-struct Item {
-    name: String,
-    count: u8,
+type Entity = Vec<Component>;
+
+enum Component {
+    Item {
+        name: String,
+    },
+    Decomposition {
+        components: Vec<Entity>,
+    },
 }
 
 fn main() {
-    let item_compendium = vec![
+    /*let item_compendium = vec![
         "Blood",
         "Water",
         "Salt",
@@ -14,8 +20,8 @@ fn main() {
         "Fibers",
         "Skeleton",
         "Circle Sigil",
-    ];
-    let mut inventory: Vec<Item> = Vec::new();
+    ];*/
+    let mut inventory: Vec<Entity> = Vec::new();
 
     println!("Welcome to Crafting Sim CLI!");
     println!("Enter \"help\" for help");
@@ -36,71 +42,9 @@ fn main() {
                 );
             }
             Some("give") | Some("g") => {
-                let mut item_name = String::new();
-                let mut item_count = 1;
-
-                let second_word = words.next();
-                if let Some(second_word) = second_word {
-                    if let Ok(number) = second_word.parse::<u8>() {
-                        item_count = number;
-
-                        if let Some(third_word) = words.next() {
-                            if let Some(fourth_word) = words.next() {
-                                item_name = third_word.to_string() + " " + &fourth_word.to_string();
-                            } else {
-                                item_name = third_word.to_string();
-                            }
-                        } else {
-                            println!("I don't know what to give you!");
-                        }
-                    } else {
-                        if let Some(third_word) = words.next() {
-                            item_name = second_word.to_string() + " " + &third_word.to_string();
-                        } else {
-                            item_name = second_word.to_string();
-                        }
-                    }
-                } else {
-                    println!("I don't know what to give you!");
-                }
-
-                let item_name = item_name.to_lowercase();
-
-                let lowercase_compendium = item_compendium
-                    .iter()
-                    .map(|x| x.to_lowercase())
-                    .collect::<Vec<_>>();
-
-                // if the item the user gave us is in the game,
-                if lowercase_compendium.contains(&item_name) {
-                    println!("It's dangerous to go alone! Take this!");
-                    println!("You recieved {} unit(s) of {}", item_count, item_name);
-
-                    let mut does_item_already_exist = false;
-
-                    for item in inventory.iter_mut() {
-                        if item.name == item_name {
-                            does_item_already_exist = true;
-                            item.count += item_count;
-                        }
-                    }
-
-                    if !does_item_already_exist {
-                        inventory.push(Item {
-                            name: item_name,
-                            count: item_count,
-                        });
-                    }
-                } else {
-                    println!("There's no such item!");
-                }
             }
 
             Some("show") | Some("s") => {
-                for item in inventory.iter() {
-                    println!("Here is a list of items you have:");
-                    println!("You have {} unit(s) of {}", item.count, item.name);
-                }
             }
 
             Some("quit") | Some("q") => break,
